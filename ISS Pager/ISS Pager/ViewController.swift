971debd098796f9675f55ec6772c110301175de9
Dedual Enterprises <nick@dedual.com>
2016-12-21 21:59:55 -0500

    Initial project created
    Created initial preliminary project after roughing out the app on
    paper.

---
+++ ISS Pager/ISS Pager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ISS Pager
+//
+//  Created by Nicolas Dedual on 12/21/16.
+//  Copyright © 2016 Dedual Enterprises Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
